f8893dc1bc211e29522598e95ea60fa90ef5156a
GonzaG <gonzagazzero2017@gmail.com>
2025-11-17 14:02:20 -0300

    definicion de apis, creacion del form login e incorporacion de bootstrap

---
+++ js/api.js
@@ -0,0 +1,5 @@
+const API_URLS = {
+    users: `https://691399ccf34a2ff1170c8f45.mockapi.io/Usuario`,
+    menu:`https://691399ccf34a2ff1170c8f45.mockapi.io/Menu`,
+    orders:`https://6913af22f34a2ff1170cdd9f.mockapi.io/orders`
+}
